348a0e03af692dd2864dec8892d2bec93f7dc659
alqusi1992 <malqusi@yahoo.com>
2021-12-11 18:25:47 +0100

    create user data with redux

---
+++ src/features/userSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    isSignIn: false,
+    userData: null,
+    searchInput: 'tech',
+    blogData: null,
+  },
+  reducers: {
+    setSignedIn: (state, action) => {
+      state.isSignIn = action.payload;
+    },
+    setUserData: (state, action) => {
+      state.userData = action.payload;
+    },
+    setInput: (state, action) => {
+      state.searchInput = action.payload;
+    },
+    setBlogData: (state, action) => {
+      state.blogData = action.payload;
+    },
+  },
+});
+
+export const { setSignedIn, setUserData, setInput, setBlogData } = userSlice.actions;
+
+export const selectIsSignIn = (state) => state.user.isSignIn;
+export const selectUserData = (state) => state.user.userData;
+export const selectSearchInput = (state) => state.user.searchInput;
+export const selectBlogData = (state) => state.user.blogData;
+
+export default userSlice.reducer;
